bc39e8b1430aa7e7109776dc9b25dfb56fa6da6b
NefdovaM <4803328@gmail.com>
2023-11-30 21:27:35 +0300

    Create filter clearing

--- src/main/java/ru/masha/Laptop.java
+++ src/main/java/ru/masha/Laptop.java
@@ -10,17 +10,25 @@ public class Laptop {
     public final int rom;
     public final int price;
     public final int colour;
+    public final int producer;
     final static HashMap<Integer, String> coloursMap = new HashMap<>() {{
         put(1, "Серебрянный");
         put(2, "Черный");
         put(3, "Белый");
     }};
+    final static HashMap<Integer, String> producersMap = new HashMap<>() {{
+        put(1, "Lenovo");
+        put(2, "ASUS");
+        put(3, "HP");
+        put(4, "Honor");
+    }};
 
-    public Laptop(int diagonal, int rom, int price, int colour) {
+    public Laptop(int diagonal, int rom, int price, int colour, int producer) {
         this.diagonal = diagonal;
         this.rom = rom;
         this.price = price;
         this.colour = colour;
+        this.producer = producer;
     }
 
     @Override
@@ -30,6 +38,7 @@ public class Laptop {
         sb.append(", оперативная память = ").append(gerPrettyRom());
         sb.append(", цена = ").append(getPrettyPrice());
         sb.append(", цвет = ").append(getPrettyColour());
+        sb.append(", производитель = ").append(getPrettyProducer());
         return sb.toString();
     }
 
@@ -49,11 +58,22 @@ public class Laptop {
         return coloursMap.containsKey(colour);
     }
 
+    public static boolean hasProducer(int producer) {
+        return  producersMap.containsKey(producer);
+    }
+
     public String getPrettyColour() {
         if (coloursMap.containsKey(colour)) {
             return coloursMap.get(colour);
         }
-        return "Неизвестный";
+        return "Неизвестный цвет";
+    }
+
+    public String getPrettyProducer() {
+        if(producersMap.containsKey(producer)){
+            return producersMap.get(producer);
+        }
+        return "Такой производитель отсутствует";
     }
 
     public static Laptop createRandomLaptop() {
@@ -69,7 +89,8 @@ public class Laptop {
                 diagonals.get(diagonalIndex),
                 (int) Math.pow(2, rnd.nextInt(7)),
                 rnd.nextInt(100_000),
-                rnd.nextInt(1, coloursMap.size() + 1)
+                rnd.nextInt(1, coloursMap.size() + 1),
+                rnd.nextInt(1, producersMap.size() + 1)
         );
     }
 }
--- src/main/java/ru/masha/LaptopShop.java
+++ src/main/java/ru/masha/LaptopShop.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Filter;
 
 public class LaptopShop {
     private final List<Laptop> laptops = new ArrayList<>();
@@ -13,17 +14,23 @@ public class LaptopShop {
     private static final int MENU_FILTER_ROM = 2;
     private static final int MENU_FILTER_PRICE = 3;
     private static final int MENU_FILTER_COLOUR = 4;
+    private static final int MENU_FILTER_PRODUCER = 5;
     private static final int MENU_ACCEPT_FILTERS = 10;
+    private static final int MENU_CLEAR_FILTERS = 11;
     private static final HashMap<Integer, String> filters = new HashMap<>() {{
         put(MENU_FILTER_DIAGONAL, "Диагональ");
         put(MENU_FILTER_ROM, "Оперативная память");
         put(MENU_FILTER_PRICE, "Цена");
         put(MENU_FILTER_COLOUR, "Цвет");
+        put(MENU_FILTER_PRODUCER, "Производитель");
     }};
     private final HashMap<Integer, Integer> userFilter = new HashMap<>();
 
     public LaptopShop() {
-        for (int i = 0; i <= 9; i++) {
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Список доступных ноутбуков");
+        System.out.println("-------------------------------------------------------------------------------");
+        for (int i = 0; i <= 14; i++) {
             laptops.add(Laptop.createRandomLaptop());
         }
         for (Laptop filteredLaptop : laptops) {
@@ -52,7 +59,12 @@ public class LaptopShop {
                     int colour = takeColourMenu();
                     userFilter.put(MENU_FILTER_COLOUR, colour);
                 }
+                case MENU_FILTER_PRODUCER -> {
+                    int producer = takeProducerMenu();
+                    userFilter.put(MENU_FILTER_PRODUCER, producer);
+                }
                 case MENU_ACCEPT_FILTERS -> acceptFilters();
+                case MENU_CLEAR_FILTERS -> clearFilters();
                 case 100 -> {
                     userFilter.forEach((id, value) -> {
                         System.out.println("введен " + filters.get(id) + " со значением " + value);
@@ -71,16 +83,22 @@ public class LaptopShop {
                 case MENU_FILTER_ROM -> filteredLaptops.removeIf((laptop) -> laptop.rom < value);
                 case MENU_FILTER_PRICE -> filteredLaptops.removeIf((laptop) -> laptop.price > value);
                 case MENU_FILTER_COLOUR -> filteredLaptops.removeIf((laptop) -> laptop.colour != value);
+                case MENU_FILTER_PRODUCER -> filteredLaptops.removeIf(laptop -> laptop.producer != value);
             }
 
         });
         if (filteredLaptops.isEmpty()) {
-            System.out.println("Нет по вашим критериям");
+            System.out.println("Нет ноутбуков по вашим критериям");
             return;
         }
         for (Laptop filteredLaptop : filteredLaptops) {
             System.out.println(filteredLaptop);
         }
+    }
+
+    private void clearFilters() {
+        userFilter.clear();
+        System.out.println("Все фильтры очищены");
     }
 
     private int takeColourMenu() {
@@ -91,18 +109,29 @@ public class LaptopShop {
         return scanner.nextInt();
     }
 
+    private int takeProducerMenu() {
+        System.out.println("Выберите производителя");
+        Laptop.producersMap.forEach((id, producer) -> {
+            System.out.println(id + " - " + producer);
+        });
+        return scanner.nextInt();
+    }
+
     private int takeInt(String msg) {
         System.out.println(msg);
         return scanner.nextInt();
     }
 
     private int mainMenu() {
+        System.out.println("-------------------------------------------------------------------------------");
         System.out.println("Введите цифру, соответствующую необходимому критерию:");
+        System.out.println("-------------------------------------------------------------------------------");
         System.out.println(MENU_EXIT + " - Выход");
         filters.forEach((id, name) -> {
             System.out.println(id + " - " + name);
         });
         System.out.println(MENU_ACCEPT_FILTERS + " - Применить фильтры");
+        System.out.println(MENU_CLEAR_FILTERS + " - Очистить фильтры");
         return scanner.nextInt();
     }
 }
